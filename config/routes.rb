bcfb89d65f60091fdf53969e339d503042cc7d29
David Hibble <139304207+hibbleD@users.noreply.github.com>
2023-10-09 16:47:56 +0000

    Photo create functioning

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   get("/users/:path_username", {:controller => "users", :action => "show"})
 
   get("/photos", {:controller => "photos", :action => "index"})
+
+  get("/photos/:path_id", {:controller => "photos", :action => "show"})
+
+  get("/delete_photo/:path_id", {:controller => "photos", :action => "destroy"})
+
+  post("/insert_photo", {:controller => "photos", :action => "create"})
 end
